5d5f4ca3574afc30f09d2edbfd269dfcf8dead80
menachemlemmer <nmmlemmer@gmail.com>
2024-07-23 16:33:12 -0400

    Create query 5

--- clues.sql
+++ clues.sql
@@ -8,33 +8,35 @@ SELECT code FROM countries WHERE population = (SELECT MIN(population) FROM count
 
 -- Write SQL query here
 
-SELECT language FROM countrylanguages WHERE countrycode = 'VAT'
+SELECT language FROM countrylanguages WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
 
-SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percentage = 100.0
+SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percentage = 100.0;
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 
-SELECT name FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino'
+SELECT name FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
 
-SELECT * FROM cities WHERE name LIKE 'Serra%' and NOT countrycode = 'SMR'
+SELECT countrycode FROM cities WHERE name LIKE 'Serra%' and NOT countrycode = 'SMR';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
+
+SELECT cities.name  FROM cities JOIN countries on countries.capital = cities.id WHERE cities.countrycode = 'BRA'
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
