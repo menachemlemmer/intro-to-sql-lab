3ed3008eeb0d576d874a49768010f53a95fa3d2a
menachemlemmer <nmmlemmer@gmail.com>
2024-07-23 16:38:40 -0400

    Create query 6 and 7

--- clues.sql
+++ clues.sql
@@ -36,7 +36,7 @@ SELECT countrycode FROM cities WHERE name LIKE 'Serra%' and NOT countrycode = 'S
 
 -- Write SQL query here
 
-SELECT cities.name  FROM cities JOIN countries on countries.capital = cities.id WHERE cities.countrycode = 'BRA'
+SELECT cities.name  FROM cities JOIN countries on countries.capital = cities.id WHERE cities.countrycode = 'BRA';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -49,7 +49,7 @@ SELECT cities.name  FROM cities JOIN countries on countries.capital = cities.id 
 --               So I'm off to add one to the population I find
 --               In a city of ninety-one thousand and now, eighty five.
 
-SELECT * FROM cities WHERE population = 91084
+SELECT * FROM cities WHERE population = 91084;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
