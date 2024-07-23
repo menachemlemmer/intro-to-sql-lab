b0fa16647ad37a5b8b7dc8182880ad76776799fd
menachemlemmer <nmmlemmer@gmail.com>
2024-07-23 15:34:49 -0400

    Create query 1 and 2

--- clues.sql
+++ clues.sql
@@ -2,10 +2,13 @@
  
 -- Write SQL query here
 
+SELECT countrycode FROM countries WHERE population = (SELECT MIN(population) FROM countries WHERE region = 'Southern Europe');
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
+
+SELECT language FROM countrylanguages WHERE countrycode = 'VAT'
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
