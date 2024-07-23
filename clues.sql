b32daf9d930e2907d1f5216445531fdf42e562fc
menachemlemmer <nmmlemmer@gmail.com>
2024-07-23 15:53:08 -0400

    Create query 4

--- clues.sql
+++ clues.sql
@@ -28,6 +28,7 @@ SELECT name FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino'
 
 -- Write SQL query here
 
+SELECT * FROM cities WHERE name LIKE 'Serra%' and NOT countrycode = 'SMR'
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
