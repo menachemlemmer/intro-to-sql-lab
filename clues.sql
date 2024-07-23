7a8b149cf582fc06187f9c70b00b036f444a59f9
menachemlemmer <nmmlemmer@gmail.com>
2024-07-23 15:50:59 -0400

    Create query 4

--- clues.sql
+++ clues.sql
@@ -22,6 +22,7 @@ SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percenta
 
 -- Write SQL query here
 
+SELECT name FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino'
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
